8755edd3396cda34c8aef5d36d98ca773e1fe12b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-17 13:59:27 +0300

    configuring ruby environment

---
+++ soar_backend/app/controllers/applicationController.rb
@@ -0,0 +1,71 @@
+class ApplicationController < Sinatra::Base
+
+    set :default_content_type, 'application/json'
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+
+
+# get '/land' do
+#   land = Land.all.order(:location)
+#   land = Land.all.order(:land_use)
+#   land.to_json
+# end
